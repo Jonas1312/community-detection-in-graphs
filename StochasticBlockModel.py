9a0fff7a26671ee684e3d6f01dff21fbb2eed562
jonas1312 <Jonas1312@users.noreply.github.com>
2017-02-02 18:39:14 +0100

    Implemented "degreesExpectations" function

--- StochasticBlockModel.py
+++ StochasticBlockModel.py
@@ -4,47 +4,57 @@
   Purpose:  Stochastic block model
   Created:  01/02/2017
 """
-import random
+from time import clock
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Stochastic block model parameters
 #----------------------------------------------------------------------
-n_vertices = 15  # number of vertices
-n_communities = 4  # number of communities
+def degreesExpectations(probability_matrix, n_communities, community_labels):
+    """Calculate communities degrees expectations"""
+    n = [len(np.argwhere(community_labels==i)) for i in xrange(n_communities)] # number of vertices per communities (n1, n2, ..., nk)
+    return np.dot(probability_matrix[:n_communities,:n_communities], n) # return [E(d1), E(d2), ..., E[d(k)]]
+
+#----------------------------------------------------------------------
+# Stochastic block model parameters
+n_vertices = 20  # number of vertices
+n_communities = 3  # number of communities
+community_labels = np.random.randint(0, n_communities, n_vertices) # community label assigned to each vertex
 probability_matrix = np.array([
-    [0.7, .1, .1, .1],
-    [.1, 0.7, .1, .1],
-    [.1, .1, 0.7, .1],
-    [.1, .1, .1, 0.7],
-]) # symetric matrix of edge probabilities
-community_labels = [random.randint(0, n_communities-1) for i in xrange(n_vertices)] # community label assigned to each vertices
+    [0.1, .01, .1, .01],
+    [.01, 0.9, .9, .01],
+    [.1, .9, 0.5, .01],
+    [.01, .01, .01, 0.1],
+]) # matrix of edge probabilities
+if(not (probability_matrix.transpose() == probability_matrix).all()):
+    raise ValueError("Probability matrix has to be symmetric")
 graph_matrix = np.zeros((n_vertices, n_vertices), dtype=int) # adjacency matrix initialization
 
-# Adjacency matrix generation
 #----------------------------------------------------------------------
+# Adjacency matrix generation (strictly lower triangular as graph is undirected)
+t0 = clock()
 for i in xrange(n_vertices):
     for j in xrange(i):
         community_i = community_labels[i]
         community_j = community_labels[j]
         val = probability_matrix[community_i][community_j]
-        p = random.random()
+        p = np.random.rand()
         if p <= val:
             graph_matrix[i][j] = 1
 
-print(graph_matrix) # print adjacency matrix
+print("Time taken to generate the graph: " + str((clock() - t0)*1000) + "ms\n")
 G = nx.from_numpy_matrix(graph_matrix) # generate networkx graph
 
-# Draw generated graph and print communities
 #----------------------------------------------------------------------
+# Draw generated graph and print communities
 color_map = ['cyan', 'red', 'yellow', 'magenta', 'blue', 'green', 'white'][:n_communities]
 node_color = [color_map[i] for i in community_labels]
-labels = {key: key+1 for key in xrange(n_vertices)}
+labels = {key: key+1 for key in xrange(n_vertices)} # vertices numbers
 nx.draw(G, labels=labels, node_color=node_color, font_size=12)
 
+degrees_expectations = degreesExpectations(probability_matrix, n_communities, community_labels)
 for i in xrange(n_communities):
     indices = [j+1 for j, x in enumerate(community_labels) if x == i]
-    print("Community C{} vertices (color: {}): {}".format(str(i), color_map[i], str(indices)))
+    print("Community C{}, color: {}, E[di] = {}".format(str(i), color_map[i], degrees_expectations[i]))
 
 plt.show()
