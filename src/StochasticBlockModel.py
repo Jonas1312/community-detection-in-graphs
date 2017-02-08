b6c2a57499cf31adf234cba0a7d09ea9456ab3ea
jonas1312 <Jonas1312@users.noreply.github.com>
2017-02-08 15:05:23 +0100

    up

--- src/StochasticBlockModel.py
+++ src/StochasticBlockModel.py
@@ -76,6 +76,7 @@ if __name__ == '__main__':
 
     #----------------------------------------------------------------------
     # Stochastic block model parameters
+    #----------------------------------------------------------------------
     n_vertices = 100  # number of vertices
     n_communities = 2  # number of communities
 
@@ -92,7 +93,7 @@ if __name__ == '__main__':
 
     #----------------------------------------------------------------------
     # Draw generated graph and print communities
-
+    #----------------------------------------------------------------------
     color_map = np.array(['cyan', 'red', 'yellow', 'magenta', 'blue', 'green', 'white'][:n_communities])
     for i in xrange(n_communities):
         indices = [j+1 for j, x in enumerate(sbm.community_labels) if x == i]
@@ -106,3 +107,33 @@ if __name__ == '__main__':
         node_color = color_map[sbm.community_labels]
         nx.draw(G, labels=labels, node_color=node_color, font_size=12)
         plt.show()
+
+    #----------------------------------------------------------------------
+    # Comparison between sparse and dense graphs
+    #----------------------------------------------------------------------
+    x = range(25,700,25)
+    y_sparse = []
+    y_dense = []
+    for i in x:
+        n_vertices = i  # number of vertices
+        n_communities = 2  # number of communities
+        cin = 13
+        cout = 3
+        probability_matrix = (1.0/n_vertices)*(np.full((n_communities,n_communities), cout, dtype=int) + np.diag([cin-cout]*n_communities))
+        sbm = SBM(n_vertices, n_communities, probability_matrix)
+        y_sparse.append(sbm.average_degree)
+    for i in x:
+        n_vertices = i  # number of vertices
+        n_communities = 2  # number of communities
+        cin = 0.7
+        cout = 0.3
+        probability_matrix = np.full((n_communities,n_communities), cout, dtype=int) + np.diag([cin-cout]*n_communities)
+        sbm = SBM(n_vertices, n_communities, probability_matrix)
+        y_dense.append(sbm.average_degree)
+    plt.xlim(0,x[-1])
+    plt.xlabel("Number of vertices")
+    plt.ylabel("Average degree")
+    plt.plot(x,y_sparse, 'r', label="sparse")
+    plt.plot(x,y_dense, 'b', label="dense")
+    plt.legend(loc='upper left')
+    plt.show()
