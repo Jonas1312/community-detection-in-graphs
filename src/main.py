cd1c087715b1c837cda5f41c9050f9a2d94e144c
jonas1312 <Jonas1312@users.noreply.github.com>
2017-02-19 17:42:03 +0100

    up

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def main():
 	#----------------------------------------------------------------------
 	# Stochastic block model parameters
 	#----------------------------------------------------------------------
-	n_vertices = 1000  # number of vertices
+	n_vertices = 10  # number of vertices
 	n_communities = 2  # number of communities
 
 	# Fixing cin > cout is referred to as the assortative case, because vertices
@@ -63,12 +63,18 @@ def main():
 	plt.title("Eigenvectors corresponding to the two smallest eigenvalues")
 	plt.plot(W[:,0], W[:,1], 'o', markersize=5)
 
-	kmeans = KMeans(n_clusters=n_clusters).fit(W)
+	kmeans = KMeans(n_clusters=n_clusters).fit(W) # kmeans
 	plt.figure()
 	plt.title("K-means")
 	for i in xrange(n_clusters):
 		ds = W[np.where(kmeans.labels_ == i)]
 		plt.plot(ds[:,0], ds[:,1], 'o', markersize=5)
+
+	if n_vertices > 300: print("Can't draw if number of vertices is too big")
+	else:
+		plt.figure()
+		plt.title("Detected communities with the Bethe Hessian matrix")
+		nx.draw(G, labels=labels, node_color=color_map[kmeans.labels_], font_size=10)
 	plt.show()
 
 	##----------------------------------------------------------------------
