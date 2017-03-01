34b1852ba0fb3d7ccda73b9a5a8f77f3b4e021ec
Alexandre <mejean.alexandre@gmail.com>
2017-03-01 11:09:51 +0100

    comment testSBM

--- src/testSBM.py
+++ src/testSBM.py
@@ -7,27 +7,33 @@ from time import clock
 import numpy as np
 import csv
 
+# ----------------------------------------------------------------------
+# Measure execution time of sbm for several values of n_vertices
+# Save results in csv file "..\execution_time.csv"
+# This csv file can be imported in excel file "..\regression_temps_execution.xlsx" to compute complexity model
+# ----------------------------------------------------------------------
+
 cin = 15
 cout = 5
 n_communities = 2  # number of communities
 
 
-n_vertices_test = [100 * n for n in xrange(1,15)]# number of vertices
+n_vertices_test = [100 * n for n in xrange(1,15)]  # number of vertices for time measures
 times_test = []
 
 for n_vertices in n_vertices_test:
     probability_matrix = (1.0 / n_vertices) * (np.full((n_communities, n_communities), cout, dtype=int) + np.diag(
         [cin - cout] * n_communities))  # matrix of edge probabilities
-    time1 = clock()
+    time1 = clock()  # start time measure
     sbm = SBM(n_vertices, n_communities, probability_matrix)
-    time2 = clock()
+    time2 = clock()  # end time measure
     times_test.append([n_vertices,(time2-time1)])
 
 plt.plot(*zip(*times_test))
 plt.title('number of vertices dependence of execution time')
 plt.show()
 
-#Save execution time in csv file
+# Save execution time in csv file
 csvfile = "..\execution_time.csv"
 
 
