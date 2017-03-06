b6553c73006b9bde0b797921b9f63c3daa2941a5
Alexandre <mejean.alexandre@gmail.com>
2017-03-06 19:44:16 +0100

    n_communities clustering test

--- src/n_communities_clustering_test.py
+++ src/n_communities_clustering_test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #coding:utf-8
 """
-  Purpose:  cin - cout spectral clustering tests
-  Created:  05/03/2017
+  Purpose:  n_communities spectral clustering tests
+  Created:  06/03/2017
 """
 
 import matplotlib.pyplot as plt
@@ -11,20 +11,22 @@ from matrices import *
 from sklearn.metrics.cluster import normalized_mutual_info_score
 from spectralClustering import SpectralClustering
 
-n_vertices = 700
+n_vertices = 800
 x_step = 0.2
-x_axis = [(15,i) for i in np.arange(4,8,x_step)]
+cin = 15
+cout = 5
+x_axis = [i for i in range(1,6)]
 yNMI_bethehessian = [0]*len(x_axis)
 yNMI_modularity = [0]*len(x_axis)
 yNMI_laplacian = [0]*len(x_axis)
 yNMI_adjacency = [0]*len(x_axis)
 
-n_communities = 2
+
 n_iterations = 1
 
 for n in xrange(n_iterations):
 	print("Iteration {}, {} left".format(n+1, n_iterations-n-1))
-	for i, (cin, cout) in enumerate(x_axis):
+	for i, n_communities in enumerate(x_axis):
 		probability_matrix = (1.0 / n_vertices) * (np.full((n_communities, n_communities), cout, dtype=int) + np.diag([cin - cout] * n_communities))
 		sbm = SBM(n_vertices, n_communities, probability_matrix)
 
@@ -50,10 +52,10 @@ for n in xrange(n_iterations):
 
 plt.title("Sparse graph, iterations: {}, communities: {}, {} vertices".format(n_iterations, n_communities, n_vertices))
 plt.ylim(0,1)
-plt.xlabel("cin - cout")
+plt.xlabel("n_communities")
 plt.ylabel("Normalized Mutual Information Score")
-x_axis = [cin-cout for cin, cout in x_axis]
-plt.plot(x_axis, [y/n_iterations for y in yNMI_bethehessian], 'r', label="Bethe-Hessian")
+
+plt.plot([y/n_iterations for y in yNMI_bethehessian], 'r', label="Bethe-Hessian")
 plt.plot(x_axis, [y/n_iterations for y in yNMI_modularity], 'b', label="Modularity")
 plt.plot(x_axis, [y/n_iterations for y in yNMI_laplacian], 'g', label="Laplacian")
 plt.plot(x_axis, [y/n_iterations for y in yNMI_adjacency], 'c', label="Adjacency")
